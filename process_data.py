e3d421a7a799501102700886a1ee57f40d42b685
HeeJeong-Yu <yhj20001206@gmail.com>
2025-11-28 16:52:32 +0900

    E: 도로명주소 컬럼 만들기

--- process_data.py
+++ process_data.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 
-def replace_to_nan(df):
+def replace_to_empty(df):
     df.replace(
-        to_replace=[0, False, None], 
-        value=np.nan,
+        to_replace=[0, False, None, np.nan], 
+        value='',
         inplace=True)
     
 
@@ -24,5 +24,35 @@ def split_eup_myeon_dong(df, col="법정읍면동명"):
 
     return df
 
+def concat_adddress(df):
+    def concat_str(col):
+        return np.where(
+            df[col].str.strip() != '', 
+            ' '+df[col], 
+            ''
+        )
+
+    address_series = df['시도명'].copy()
+    address_series += concat_str('시군구명')
+    address_series += concat_str('법정읍면')
+
+    address_series += ' ' + df['도로명'] + ' ' + df['건물본번']
+
+    address_series += np.where(
+        df['건물부번'].str.strip() != '', 
+        '-' + df['건물부번'], 
+        ''
+    )
+
+    return address_series
 
 def process_data(df):
+    replace_to_empty(roadname_df)
+    roadname_df = roadname_df.astype(str)
+
+    # 법정읍면동명-> 법정동, 법정읍면으로 분리
+    roadname_df = split_eup_myeon_dong(roadname_df)
+    roadname_df = roadname_df.drop(columns=['법정읍면동명'])
+
+    # 도로명주소 
+    roadname_df['도로명주소'] = concat_adddress(roadname_df)
